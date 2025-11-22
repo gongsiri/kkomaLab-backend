ccaf4e40a48a3bbeeb36dccad1d6cb266e1410f4
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-11-22 21:05:02 +0900

    feat: Create prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SensorModule } from './sensor/sensor.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SensorModule],
+  imports: [SensorModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
