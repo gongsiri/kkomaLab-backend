0d87ca24bd18176898e638bedf3bb8e402b8fcc9
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-12-10 20:15:30 +0900

    feat: Install WebSocket

---
+++ src/dashboard/dashboard.gateway.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from '@nestjs/config';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: { origin: process.env.CORS_ORIGINS?.split(','), credentials: true },
+})
+export class DashboardGateway {
+  @WebSocketServer()
+  server: Server;
+
+  public afterInit(): void {
+    console.log('[WebSocket] Server initialized');
+  }
+
+  public handleConnect(client: Socket): void {
+    console.log(`[WebSocket] Client connected: ${client.id}`);
+  }
+
+  public handleDisconnect(client: Socket): void {
+    console.log(`[WebSocket] Client disconnected: ${client.id}`);
+  }
+}
