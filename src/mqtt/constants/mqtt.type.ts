6bd84950a8243c0fcbc026eef0b386538acd4789
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-12-14 19:07:37 +0900

    fix: Change the name of mqtt type

--- src/mqtt/constants/mqtt.type.ts
+++ src/mqtt/constants/mqtt.type.ts
@@ -2,11 +2,11 @@ export enum MqttType {
   TEMPERATURE = 'temperature',
   HUMIDITY = 'humidity',
   CO2 = 'co2',
-  MOTION_DETECT = 'jy-kkomalab-9988/room1/motion-detect',
+  MOTION_DETECT = 'motion-detect',
 
   FAN_STATE = 'fan/state',
-  LIGHT_STATE = 'jy-kkomalab-9988/room1/light/state',
+  LIGHT_STATE = 'light/state',
 
   FAN_COMMAND = 'fan/cmd',
-  LIGHT_COMMAND = 'jy-kkomalab-9988/room1/light/cmd',
+  LIGHT_COMMAND = 'light/cmd',
 }
